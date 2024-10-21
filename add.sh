e4bff2d98c54a818828ce9f19aa63d43a31b98c3
j4nu52gr2zyb68 <januszgrzyb68@gazeta.pl>
2024-10-21 20:53:36 +0200

    new script

--- add.sh
+++ add.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 yt-dlp -x --audio-format=opus $1 &&
-filename=$(find . -maxdepth 1 -type f -name "*.opus" | sed -E "s/.\///") &&
+filename=$(find . -maxdepth 1 -type f -name "*.opus" | sed -E "s/.\///")
+filename_url=$(echo $filename | sed "s/ /%20/g") &&
 echo $filename &&
-mv *.opus ./assets/audio/music &&
-sed -i "s/<file-list>/<file-list>\n\t\t$filename/" index.html 
+mv *.opus ./music &&
+sed -i "s/<file-list>/<file-list>\n\t\t\t<a href=https:\/\/github.com\/j4nu52gr2yb\/muzyka\/music\/$filename_url>$filename<\/a>/" index.html 
