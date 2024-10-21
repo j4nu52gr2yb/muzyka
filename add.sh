c4fb855c81f2b50ce8a7344c33100792235feabc
j4nu52gr2zyb68 <januszgrzyb68@gazeta.pl>
2024-10-21 21:01:41 +0200

    2

--- add.sh
+++ add.sh
@@ -4,4 +4,4 @@ filename=$(find . -maxdepth 1 -type f -name "*.opus" | sed -E "s/.\///")
 filename_url=$(echo $filename | sed "s/ /%20/g") &&
 echo $filename &&
 mv *.opus ./music &&
-sed -i "s/<file-list>/<file-list>\n\t\t\t<a href=https:\/\/github.com\/j4nu52gr2yb\/muzyka\/blob\/main\/music\/$filename_url>$filename<\/a>/" index.html 
+sed -i "s/<file-list>/<file-list>\n\t\t\t<a href=https:\/\/raw.githubusercontent.com/j4nu52gr2yb\/muzyka\/refs\/heads\/main\/music\/$filename_url>$filename<\/a>/" index.html 
