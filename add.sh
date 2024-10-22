57b8a00ab3643f215b8f02d599c1df3d2400377e
j4nu52gr2yb <januszgrzyb63@gazeta.pl>
2024-10-22 17:18:23 +0200

    bug

--- add.sh
+++ add.sh
@@ -4,4 +4,4 @@ filename=$(find . -maxdepth 1 -type f -name "*.opus" | sed -E "s/.\///")
 filename_url=$(echo $filename | sed "s/ /%20/g")
 sed_url=$(echo $1 | sed "s./.\\\/.g")
 mv *.opus ./music &&
-sed -i "s/<file-list>/<file-list>\n\t\t\t<a href=https:\/\/raw.githubusercontent.com\/j4nu52gr2yb\/muzyka\/refs\/heads\/main\/music\/$filename_url>$filename<\/a>: <a href=$sed_url>$sed_url<\/a>i<\/br>/" index.html # mogles uzyc idioto zamiasy / operatora . w sedzie
+sed -i "s/<file-list>/<file-list>\n\t\t\t<a href=https:\/\/raw.githubusercontent.com\/j4nu52gr2yb\/muzyka\/refs\/heads\/main\/music\/$filename_url>$filename<\/a>: <a href=$sed_url>$sed_url<\/a><\/br>/" index.html # mogles uzyc idioto zamiasy / operatora . w sedzie
