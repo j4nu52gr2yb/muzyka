8f55fb7bae54a969a22d7c06e17ac297452234b5
j4nu52gr2zyb68 <januszgrzyb68@gazeta.pl>
2024-10-22 22:56:46 +0200

    encode to 64kbps

--- add.sh
+++ add.sh
@@ -3,5 +3,7 @@ yt-dlp -x --audio-format=opus $1 &&
 filename=$(find . -maxdepth 1 -type f -name "*.opus" | sed -E "s/.\///")
 filename_url=$(echo $filename | sed "s/ /%20/g")
 sed_url=$(echo $1 | sed "s./.\\\/.g")
-mv *.opus ./music &&
+ffmpeg -i $filename -b:a 64k temp.opus &&
+rm $filename &&
+mv temp.opus ./music/$filename &&
 sed -i "s/<file-list>/<file-list>\n\t\t\t<a href=https:\/\/raw.githubusercontent.com\/j4nu52gr2yb\/muzyka\/refs\/heads\/main\/music\/$filename_url>$filename<\/a>: <a href=$sed_url>$sed_url<\/a><\/br>/" index.html # mogles uzyc idioto zamiasy / operatora . w sedzie
