5f1e69e59975a6c0a96b3225b8454f6b761a1aa0
j4nu52gr2yb <januszgrzyb63@gazeta.pl>
2024-10-23 07:42:05 +0200

    na razie beda nieskompresowane

--- add.sh
+++ add.sh
@@ -3,8 +3,8 @@ yt-dlp -x --audio-format=opus $1 &&
 filename=$(find . -maxdepth 1 -type f -name "*.opus" | sed -E "s/.\///")
 filename_url=$(echo $filename | sed "s/ /%20/g")
 sed_url=$(echo $1 | sed "s./.\\\/.g")
-ffmpeg -i "$filename" -b:a 96k -c:a libopus temp.opus && # 96 bo i tak konwertuje do ogg
-rm "$filename" &&
-mv temp.opus "./music/$filename" &&
-#mv *.opus ./music &&
+#ffmpeg -i "$filename" -b:a 96k -c:a libopus temp.opus && # 96 bo i tak konwertuje do ogg
+#$rm "$filename" &&
+#mv temp.opus "./music/$filename" &&
+mv *.opus ./music &&
 sed -i "s/<file-list>/<file-list>\n\t\t\t<a href=https:\/\/raw.githubusercontent.com\/j4nu52gr2yb\/muzyka\/refs\/heads\/main\/music\/$filename_url>$filename<\/a>: <a href=$sed_url>$sed_url<\/a><\/br>/" index.html # mogles uzyc idioto zamiasy / operatora . w sedzie
